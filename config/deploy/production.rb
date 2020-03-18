b565b9821da7f4d4e4ad5c85576a677f890e0eab
van319 <bansyuukatu@gmail.com>
2020-03-18 15:43:30 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,9 +49,7 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
- server "3.113.131.137",
-   user: "ec2-user",
-   roles: %w{web app},
+server "3.113.131.137",user: "ec2-user",roles: %w{web app},
 #   ssh_options: {
 #     user: "user_name", # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
