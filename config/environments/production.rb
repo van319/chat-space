6d4f4500746d8ee90e48ddc77f4f86aaafa4fe5c
van319 <bansyuukatu@gmail.com>
2020-03-18 19:42:00 +0900

    edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Disable serving static files from the `/public` folder by default since
@@ -60,7 +60,7 @@ Rails.application.configure do
 
 
 
-  
+
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
